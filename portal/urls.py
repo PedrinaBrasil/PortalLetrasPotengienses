89e12200a626583d938352859cabc2aff914bbc9
PedrinaBrasil <pedrinabrasil@gmail.com>
2021-07-30 08:43:18 -0300

    links do menu inicial enviando para listas

--- portal/urls.py
+++ portal/urls.py
@@ -6,5 +6,5 @@ app_name = "portal"
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('obras', views.BooksListView.as_view(), name="obras"),
-    path('autores', views.AuthorsListView.as_view(), name="obras"),
+    path('autores', views.AuthorsListView.as_view(), name="autores"),
 ]
